c39669b1fcc2062d57b8be2fee68b43408fd5caa
sugandhasharma12 <121653989+sugandhasharma12@users.noreply.github.com>
2024-08-02 18:37:02 +0000

    basics

---
+++ basics.js
@@ -0,0 +1,23 @@
+"use strict"//treat all js code as new
+//alert(3+3);//we are using nodejs,not browser
+
+//DATA TYPE
+let name="sugandha"
+let age=18
+let isLoggedIn=false
+
+//number=>2to power 53
+//bigint
+//string=>""
+// boolean=>true/false
+// null=>standalone value//object
+// undefined=>
+    // symbol=>unique
+
+//object
+console.log(typeof"sugandha");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof "undefined");
+console.log(typeof null);//object
+console.log(typeof "null");
