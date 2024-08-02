c3cd27ba7c98ff1897b6ca6c9f946d82c4ef26f9
sugandhasharma12 <121653989+sugandhasharma12@users.noreply.github.com>
2024-08-02 19:05:43 +0000

    dataconversion

---
+++ new/datatypeconversion.js
@@ -0,0 +1,16 @@
+let score=33;
+console.log(typeof score);
+console.log(typeof (score));
+let sc="33";
+console.log(typeof sc);
+let valueINnumber=Number(sc);
+console.log(typeof valueINnumber);
+let s="ANC123";
+let valuenumber=Number(s);
+console.log(typeof valuenumber);
+console.log(valuenumber);//Nan
+
+let checknull=null;
+let numbe=Number(checknull);
+console.log(typeof checknull);
+console.log(numbe);
